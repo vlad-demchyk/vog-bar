5a56bacbd56b7ce4aa2defd9c786f75b66ecadc4
andrii-neroznak-eyewa <andrii.neroznak@eyewa.com>
2024-12-08 17:25:00 +0100

    WIP

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -23,3 +23,4 @@ const analytics = getAnalytics(app);
 const database = getFirestore()
 
 export default database
+
